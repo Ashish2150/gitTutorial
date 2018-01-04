2eabc7e5ecf28cf5fd21330ae26c6a929c94b03b
Ashish2150 <ashishmauryait@gmail.com>
2018-01-05 00:56:17 +0530

    Init Commit.

---
+++ gitTutorial/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  gitTutorial
+//
+//  Created by Ashish Kumar Mourya on 05/01/18.
+//  Copyright © 2018 Ashish Kumar Mourya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var btnNew: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
